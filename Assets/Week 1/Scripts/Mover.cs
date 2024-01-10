17a4b96445a0493f25c72bef60e04872817a660b
Chanmin <parchanm@sheridancollege.ca>
2024-01-10 17:44:14 -0500

    Task 2 a completed: tank moving with keyboard input.

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 3f;
+    public GameObject missilePrefab;
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        transform.Translate(direction * speed * Time.deltaTime , 0, 0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab, spawn.position, spawn.rotation);
+        }
+    }
+}
