54ad7f6f8dc783dd263bef79d6aa3b7086cc73b7
Chanmin <parchanm@sheridancollege.ca>
2024-01-17 09:43:57 -0500

    PLayer moving by force

---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force = 10f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction *force * Time.deltaTime);
+    }
+}
