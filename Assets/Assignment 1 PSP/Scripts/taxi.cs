69e6f5293b189f2bc2c891847018e201eaaf7364
Chanmin <parchanm@sheridancollege.ca>
2024-01-27 21:11:29 -0500

    Assignment 1 Save 2: wrote car script, got car physics working. getting ready to make ontrigger and oncollision

--- Assets/Assignment 1 PSP/Scripts/taxi.cs
+++ Assets/Assignment 1 PSP/Scripts/taxi.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class taxi : MonoBehaviour
 {
+    public Rigidbody2D taxiRigidbody;
+
+    //public float rotation;
+    public float jumpPower = 10;
+    public float speed = 5;
+    public float rotationSpeed = 50;
+    public float maxSpeed = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //float turning = rotation = Input.GetAxis("Vertical");
+        //transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+
+        float direction = Input.GetAxis("Horizontal");
+        float rotation = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, 0, rotation * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            taxiRigidbody.velocity = Vector2.up * jumpPower;
+        }
+
+        if (taxiRigidbody.velocity.magnitude < maxSpeed)
+        {
+            taxiRigidbody.AddForce(new Vector2(direction * speed, 0));
+        }
     }
+    private void FixedUpdate()
+    {
+       //taxiRigidbody.AddTorque(rotation * rotationSpeed * Time.deltaTime);
+    }
+
 }
+
